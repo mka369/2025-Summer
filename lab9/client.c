5c1a22bee702389500f979e5ea68bbd6460be831
Minseo Kim <mka277@sfu.ca>
2025-07-27 18:00:42 -0700

    CMPT 201 Lab9 submodule removed

---
+++ lab9/client.c
@@ -0,0 +1,68 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT 8000
+#define BUF_SIZE 64
+#define ADDR "127.0.0.1"
+
+#define handle_error(msg)                                                      \
+  do {                                                                         \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+/*
+Questions to answer at top of client.c:
+(You should not need to change the code in client.c)
+1. What is the address of the server it is trying to connect to (IP address and port number).
+   : 127.0.0.1 (local)
+2. Is it UDP or TCP? How do you know?
+   : TCP. It uses SOCK_STREAM for the socket type.
+3. The client is going to send some data to the server. Where does it get this data from? How can you tell in the code?
+   : It gets the data from command line input. Line 54 reads from STDIN_FILENO.
+4. How does the client program end? How can you tell that in the code?
+   : The client ends if it receives an empty input. Line 54 implies num_read must be greater than 1
+   to perform write().
+*/
+
+int main() {
+  struct sockaddr_in addr;
+  int sfd;
+  ssize_t num_read;
+  char buf[BUF_SIZE];
+
+  sfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sfd == -1) {
+    handle_error("socket");
+  }
+
+  memset(&addr, 0, sizeof(struct sockaddr_in));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(PORT);
+  if (inet_pton(AF_INET, ADDR, &addr.sin_addr) <= 0) {
+    handle_error("inet_pton");
+  }
+
+  int res = connect(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
+  if (res == -1) {
+    handle_error("connect");
+  }
+
+  while ((num_read = read(STDIN_FILENO, buf, BUF_SIZE)) > 1) {
+    if (write(sfd, buf, num_read) != num_read) {
+      handle_error("write");
+    }
+    printf("Just sent %zd bytes.\n", num_read);
+  }
+
+  if (num_read == -1) {
+    handle_error("read");
+  }
+
+  close(sfd);
+  exit(EXIT_SUCCESS);
+}
